d8a769d3d9f96743f999c835b7f4844fb0b69351
Paul Wheeler <paul@invioinc.com>
2019-07-03 15:32:20 -1000

    Added resume.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
+const moment = require('moment');
+const handlebars = require('handlebars');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
 
   eleventyConfig.addHandlebarsHelper(
     "traverse",
-    function (context, options) {
+    function (object, options) {
       let key = options.hash["children"];
-      if (context) {
+      if (object) {
         let rendered = "";
-        let queue = context instanceof Array ? context.slice() : [context];
+        let queue = object instanceof Array ? object.slice() : [object];
 
         while (queue.length) {
           let next = queue.shift();
@@ -20,6 +23,111 @@ module.exports = function(eleventyConfig) {
 
         return rendered;
       }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "if-any",
+    function () {
+      let conditions = Array.from(arguments).slice(0, -1);
+      let options = arguments[arguments.length - 1];
+      let value = conditions.filter(v => v)[0];
+      if (value) {
+        return options.fn({ ...this, value });
+      } else if (options.inverse) {
+        return options.inverse();
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$coalesce",
+    function () {
+      let ary = Array.from(arguments).slice(0, -1);
+      if (ary instanceof Array) {
+        for (let item of ary) {
+          if (item) {
+            return item;
+          }
+        }
+
+        return undefined;
+      } else {
+        return ary;
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$lower",
+    function (str, options) {
+      if (typeof str === "string") {
+        return str.toLocaleLowerCase();
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$upper",
+    function (str, options) {
+      if (typeof str === "string") {
+        return str.toLocaleUpperCase();
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$strip",
+    function (str, chars) {
+      if (str && chars) {
+        let charArray = Array.from(chars);
+        let result = '';
+        for (let i = 0; i < str.length; i++) {
+          if (!charArray.includes(str[i])) {
+            result += str[i];
+          }
+        }
+        return result;
+      } else {
+        return str;
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$formatDate",
+    function (str, format) {
+      if (str && format) {
+        return moment(str).format(format);
+      } else {
+        return str;
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$abbr",
+    function (obj, className) {
+      if (obj && obj.abbreviation) {
+        let classAttr =
+          typeof className === "string" ?
+            ` class="${className}"` :
+            '';
+        return new handlebars.SafeString(
+          `<abbr title="${obj.text}"${classAttr}>${obj.abbreviation}</abbr>`
+        )
+      } else {
+        return obj;
+      }
+    }
+  );
+
+  eleventyConfig.addHandlebarsHelper(
+    "$log",
+    function (value) {
+      console.log((typeof value) + (value && value.prototype ? ': ' + value.prototype.constructor.name : ''));
+      console.log(value);
+      return value;
     }
   );
 
